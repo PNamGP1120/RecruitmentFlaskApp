8b35983be82be4449c44502122c55b30c4b6453d
pnam <phuongnam.it0212@gmail.com>
2025-07-22 14:19:01 +0700

    merge main

--- app/index.py
+++ app/index.py
@@ -464,9 +464,10 @@ def webhook():
         print("❌ Error in webhook:", str(e))
         return jsonify({"error": str(e)}), 400
 
+
 if __name__ == '__main__':
     with app.app_context():
         from app.admin import *
 
-        app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=3000)
 
