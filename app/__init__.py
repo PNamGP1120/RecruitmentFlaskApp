3a4837c6b660ece879bf41e9084009bfcfc0d6c9
PNamGP1120 <phuongnam.it0212@gmail.com>
2025-08-18 12:55:32 +0700

    fix gg login

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='jikagfvcuyidwsgvvudbsjahfduyjfvdguieygvsfuy'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/recruitmentdb?charset=utf8mb4' % quote("Admin@123")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/recruitmentdb?charset=utf8mb4' % quote("Phuongnam0212@")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config["PAGE_SIZE"] = 8
 
