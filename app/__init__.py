f4670111d314518849e0e84037ad0d5d5eafc8e8
EvilLocDev <nguyentuanloc15022004@gmail.com>
2025-08-31 22:51:39 +0700

    env configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 import cloudinary
+from dotenv import load_dotenv
 from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
@@ -9,18 +10,19 @@ from flask_login import LoginManager
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_socketio import SocketIO
 
+load_dotenv()
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jikagfvcuyidwsfgdsfhahfadgdhdfhbssgvvudbsjahfduyjfvdguieygvsfuy'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/recruitmentdb?charset=utf8mb4' % quote(
-    "Admin@123")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_password = quote(os.getenv('DB_PASSWORD'))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{db_password}@localhost/recruitmentdb?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config["PAGE_SIZE"] = 8
 
-app.config["GOOGLE_OAUTH_CLIENT_ID"] = "310143509450-erjdeni7iprik2u725hkil1i3a9dg9ud.apps.googleusercontent.com"
-app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = "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"
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
 # app.config["GOOGLE_REDIRECT_URI"] = "http://localhost:5000/login/callback"
 
 google_bp = make_google_blueprint(
@@ -42,9 +44,9 @@ login = LoginManager(app)
 socketio = SocketIO(app)
 
 cloudinary.config(
-    cloud_name="dqpu49bbo",
-    api_key="743773348627895",
-    api_secret="EF7elKsibuI8JEBqfMNZYYWUYvo",
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
     secure=True
 )
 
@@ -52,9 +54,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'maivo0902@gmail.com'
-app.config['MAIL_PASSWORD'] = 'qzha nhir cldl ypzy'
-app.config['MAIL_DEFAULT_SENDER'] = 'maivo0902@gmail.com'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
 mail = Mail(app)
 
